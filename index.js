3a7bf74de8f5eb0d6cd67bb40b30a9022afd0cae
lucianovilela <lucianovilela@gmail.com>
2021-04-01 14:39:06 +0000

    wip

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ require("./db/mongoose");
 const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
+const morgan = require("morgan");
 const app = express();
 app.use(cors());
+app.use(morgan(":method :url :response-time"));
 app.use(authRoutes);
+const { auth } = require("./middlewares/auth");
+app.use("/private", auth );
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
